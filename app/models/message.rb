6d34d0fa4008b36780a50af78e72ae7c03926b84
GuyGinat <guyginat4@gmail.com>
2025-07-24 00:29:10 -0400

    created initial models

---
+++ app/models/message.rb
@@ -0,0 +1,13 @@
+class Message
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :content, type: String
+  field :to, type: String
+  field :from, type: String
+  field :status, type: String
+  field :sent_at, type: Time
+  field :twilio_sid, type: String
+  field :session_id, type: String
+  field :error_message, type: String
+  belongs_to :user
+end
